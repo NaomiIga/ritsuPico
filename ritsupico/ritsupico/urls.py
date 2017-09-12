9ed8bd84c9637e1b4a4b0699bec1a35900c1191d
royroy55 <rotsuki@mxdlab.net>
2017-09-13 01:29:13 +0900

    debug

--- ritsupico/ritsupico/urls.py
+++ ritsupico/ritsupico/urls.py
@@ -18,11 +18,12 @@ from django.contrib import *
 #from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
+from django.conf.urls import include, url
 from django.conf import settings
 from cms import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^',include('django.contrib.staticfiles.urls')),
     url(r'^pico_login', views.pico_login, name="pico_login"),
     url(r'^hint', views.hint, name="hint"),
