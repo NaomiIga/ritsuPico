04cc417e7b0bdadc10eb47002f08841f5d1645e1
伊賀尚美 <iganaomi@iga-MacBook-Pro.local>
2017-09-13 15:41:26 +0900

    admin

--- ritsupico/cms/admin.py
+++ ritsupico/cms/admin.py
@@ -17,20 +17,22 @@ class UserAdmin(admin.ModelAdmin):
 class UsedHintAdmin(admin.ModelAdmin):
 	list_display = ('username',)
 	list_display_links = ('username',)
-
+'''
 class BeaconAdmin(admin.ModelAdmin):
 	list_display = ('beacon_id', 'uuid', 'major', 'minor', 'category',)
 	list_display_links = ('beacon_id',)
+'''
 '''
 class Shop_BeaconAdmin(admin.ModelAdmin):
 	list_display = ('shopname', 'shop_id', 'major', 'minor', 'floor')
 	list_display_links = ('shopname', 'shop_id', 'major', 'minor',)
 	search_fields = ['shopname', 'floor']
 '''
+'''
 class TreasureAdmin(admin.ModelAdmin):
 	list_display = ('treasure', 'beacon_id',)
 	list_display_links = ('treasure', 'beacon_id',)
-
+'''
 class HintAdmin(admin.ModelAdmin):
 	list_display = ('treasure_num', 'hint_num', 'hint_sent',)
 	list_display_links = ('hint_num', 'hint_sent',)
@@ -38,8 +40,8 @@ class HintAdmin(admin.ModelAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(UsedHint, UsedHintAdmin)
-admin.site.register(Beacon, BeaconAdmin)
+#admin.site.register(Beacon, BeaconAdmin)
 #admin.site.register(Shop_Beacon, Shop_BeaconAdmin)
-admin.site.register(Treasure_Beacon, TreasureAdmin)
+#admin.site.register(Treasure_Beacon, TreasureAdmin)
 admin.site.register(Hint, HintAdmin)
 #admin.site.register(Data, DataAdmin)
