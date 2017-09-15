48ca22b523637d412765eff35fd3c3a6966a6483
伊賀尚美 <iganaomi@iga-MacBook-Pro.local>
2017-09-15 14:54:21 +0900

    views

--- ritsupico/cms/views.py
+++ ritsupico/cms/views.py
@@ -629,6 +629,8 @@ def recover_data2(request):
 
 		#key_number = UserData.key_num + 1
 		key_number = UserData.key_num
+		if key_number < 0:
+			key_number = 0
 		print "key_num"
 		print key_number
 		#sys.stderr.write("message")
